673c1a83e947ed271723c8232f812d753a590dff
William Johnson <wlpj2011@gmail.com>
2025-10-24 23:33:00 -0400

    Created push_front for doubly linked list

--- lists/src/fourth.rs
+++ lists/src/fourth.rs
@@ -1,9 +1,9 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 pub struct List<T> {
     head: Link<T>,
-    tail: Link<T>
+    tail: Link<T>,
 }
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
@@ -16,12 +16,53 @@ struct Node<T> {
 
 impl<T> Node<T> {
     fn new(elem: T) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Node {elem, next: None, prev: None}))
+        Rc::new(RefCell::new(Node {
+            elem,
+            next: None,
+            prev: None,
+        }))
     }
 }
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None}
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else needs +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // Non-empty list, need to connect new_head to old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+            }
+            None => {
+                // Empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+            }
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        // need to take the old head, ensuring it's -2
+        self.head.take().map(|old_head| {
+            // -1 old
+            match old_head.borrow_mut().next.take() {
+                Some(_) => {
+                    todo!()
+                }
+                None => {
+                    todo!()
+                }
+            }
+            old_head.into_inner().elem
+        })
     }
 }
